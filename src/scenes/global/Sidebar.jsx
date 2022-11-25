36307cd365eda0b13744f1bbd4fb6af3845aad51
richardwaters9049 <119029825+richardwaters9049@users.noreply.github.com>
2022-11-25 05:35:51 +0000

    add files

---
+++ src/scenes/global/Sidebar.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import {Box, IconButton, Typography, useTheme} from '@mui/material';
+import { Link } from 'react-router-dom';
+import { tokens } from '../../theme';
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+
+const Sidebar = () => {
+    const theme = useTheme(); 
+    const colors = (theme.palette.mode); 
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    return <div>Sidebar</div>
+}
+
+export default Sidebar; 
